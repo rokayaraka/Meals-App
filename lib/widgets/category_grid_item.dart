e9b0103a5e5dc1fc4a14f6ccb286a6b0dc21c76a
rokayaraka <rokayaraka251@gmail.com>
2025-09-20 22:22:14 +0600

    v1

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/Category.dart' as myCategory;
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final myCategory.Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+          color: Theme.of(context).colorScheme.onBackground,
+        ),
+      ),
+    );
+  }
+}
