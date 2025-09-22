ab3641e2a4b24915176445120a708d0858175505
rokayaraka <rokayaraka251@gmail.com>
2025-09-23 03:05:30 +0600

    update mealItem tarit

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -9,6 +10,15 @@ class MealItem extends StatelessWidget {
   });
 
   final Meal meal;
+  String get ComplexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+   String get AffordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +67,31 @@ class MealItem extends StatelessWidget {
                     const SizedBox(
                       height: 12,
                     ),
-                    Row(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          //label: meal.duration.toString(),
+                          label: '${meal.duration} min',
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          //label: meal.duration.toString(),
+                          label: ComplexityText,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: AffordabilityText ,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
